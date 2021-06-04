652556ec9e9472c3ebbac5835f2e25ac18782698
Your Name <hakanor99@gmail.com>
2021-06-04 13:34:32 +0300

    Components added to NewActivity (work on progress)

--- app/src/main/java/com/hakanor/todolist/MainActivity.java
+++ app/src/main/java/com/hakanor/todolist/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
--- app/src/main/java/com/hakanor/todolist/NewActivity.java
+++ app/src/main/java/com/hakanor/todolist/NewActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -16,7 +17,8 @@ public class NewActivity extends AppCompatActivity {
 
     private EditText editText1;
     private EditText editText2;
-    private Button button;
+    private ImageView edit_button;
+    private ImageView check_button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,15 +33,25 @@ public class NewActivity extends AppCompatActivity {
         editText2 = findViewById(R.id.editText2);
         editText1.setText(mExampleList.get(position).getText1());
         editText2.setText(mExampleList.get(position).getText2());
-        button = findViewById(R.id.button);
+        edit_button=findViewById(R.id.edit_button);
+        check_button=findViewById(R.id.check_button);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        edit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editText1.setEnabled(true);
                 editText2.setEnabled(true);
+                check_button.setVisibility(1);
             }
         });
+
+        check_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // bu buton kaydetme ile ilgili işlerde lazım.
+            }
+        });
+
 
     }
 }
